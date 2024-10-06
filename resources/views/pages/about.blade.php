531f53472c6dc4b746dcbb549b2c8936bd5b78b4
Corfaya <lauracorrado0696@gmail.com>
2024-10-06 15:56:02 +0200

    [UPD test layout with first card

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,9 +10,15 @@
         </div>
         <div class="row">
             {{-- qui andranno le bio --}}
-            <div class="col-12 col-md-6">
-                <div class="img-box">
-                    <img src="{{Vite::asset('resources/img/piero.jpg')}}" alt="">
+            <div class="col-12">
+                <div class="d-flex justify-content-center align-items-center my-5">
+                    <div class="img-box">
+                        <img src="{{Vite::asset('resources/img/piero.jpg')}}" alt="Medieval developer">
+                    </div>
+                    <div class="info-box px-5">
+                        <h2 class="text-center fw-bolder">Sir Piero Bramanti</h2>
+                        <p>Nato e cresciuto in quel di Pesaro, tra le onde dell'Adriatico mare. Maestro nell’arte della sfoglia, la sua mano stende la pasta con tal grazia che persino le anziane dame di gran lignaggio del paese non vi si posson paragonare. Le sue passioni, nella quiete del tempo libero, sono le storie narrate e le melodie diffuse dalle onde eteree dei menestrelli. Predilige una serata tranquilla tra le mura domestiche, dove si impegna in epiche battaglie simulate, piuttosto che partecipare ai frenetici festeggiamenti delle danze notturne. Si considera un gigante, dotato di un cuore tenero e affettuoso.</p>
+                    </div>
                 </div>
             </div>
             <div class="col-12 col-md-6">
