41c8669f78535abb074c5df5739791c4fe07eb0e
pierobramanti <piero.bramanti94@gmail.com>
2024-10-07 15:15:04 +0200

    [UPD] created show and index and apllied style in scss partials

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -1,13 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
     <div class="row">
-        <div class="col-12">
-            
+        <div class="col-12 index-page"> 
+            <!-- Pulsante per aggiungere un nuovo personaggio -->
+            <div class="text-center mb-3">
+                <a href="{{ route('characters.create') }}" class="btn btn-primary">Add New Character</a>
+            </div>
+
+            <!-- Lista dei personaggi -->
+            <div class="list-group">
+                <!-- Ciclo sui personaggi -->
+                @foreach($character as $character)
+                <div class="list-group-item d-flex justify-content-between align-items-center">
+                    <!-- Nome del personaggio a sinistra -->
+                    <span class="fs-2">{{ $character->name }}</span>
+
+                    <!-- Pulsanti a destra -->
+                    <div>
+                        <!-- Link per visualizzare i dettagli del personaggio -->
+                        <a href="{{ route('characters.show', ['character' => $character->id]) }}" class="btn btn-info btn-sm">Show</a>
+
+                        <!-- Link per modificare il personaggio -->
+                        <a href="{{ route('characters.edit', ['character' => $character->id]) }}" class="btn btn-warning btn-sm">Edit</a>
+
+                        <!-- Form per eliminare il personaggio -->
+                        <form action="{{ route('characters.destroy', ['character' => $character->id]) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this character?')">Delete</button>
+                        </form>
+                    </div>
+                </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
-
-@endsection('content')
+@endsection
