c842013d723f16e58d6682c8e28ea4a36b6c0ac8
Corfaya <lauracorrado0696@gmail.com>
2024-10-08 19:20:18 +0200

    [UPD]] new folders and files order

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -37,34 +37,8 @@
                         </li>
                     </ul>
                 </div>
-                {{-- Test offcanvas BS --}}
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="mobileMenu" aria-labelledby="mobileMenuLabel">
-                    <div class="offcanvas-header">
-                        <h2 class="offcanvas-title fw-bolder" id="mobileMenuLabel">Menu</h2>
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <ul>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link" href="#mobileCollapse" role="button" data-bs-toggle="collapse" aria-expanded="false" aria-controls="mobileCollapse">
-                                  Database
-                                </a>
-                                <div class="collapse p-0" id="mobileCollapse">
-                                    <ul class="mt-2">
-                                        <li><a href="{{ route('characters.index') }}">Personaggi</a></li>
-                                        <li><a href="">Oggetti</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <a href="{{ route('about') }}">Su di noi</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('contacts') }}">Contatti</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+
+                @include('partials.offcanvas')
             </div>
         </div>
     </div>
