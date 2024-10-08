426dc537fe44da274f31adc87ecb38febaffc619
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-08 18:26:29 +0200

    [UPD] first setup create page

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -4,17 +4,20 @@
 
     <section id="create_page">
         <div class="container text-center">
+            <h1 class="bg-warning mb-3">
+                Genera qui il tuo combattente!
+            </h1>
             <div class="d-flex justify-content-center">
-                <h1 class="align-self-center font_fam fit_content">
-                    Genera qui il tuo combattente!
+                <h1 class="align-self-center fit_content w-25">
+                    Qui, prima o poi, si potra' caricare l'immagine del pg che si vorra' creare
                 </h1>
                 <form action="{{ route('characters.store') }}" method="post" class="w-75">
                     @csrf
-                    <div class="row">
+                    <div class="row text-white">
                         <div class="col-4">
                             <div class="form-group py-2">
-                                <label for="name">Name</label>
-                                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                                <label for="name" class="fs-2 fw-semibold">Nome</label>
+                                <input type="text" class="form-control rounded-0 @error('name') is-invalid @enderror" id="name"
                                     name="name" value="{{ old('name') }}" required>
                                 @error('name')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -23,8 +26,8 @@
                         </div>
                         <div class="col-8">
                             <div class="form-group py-2">
-                                <label for="description">Description</label>
-                                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                                <label for="description" class="fs-2 fw-semibold">Descrizione</label>
+                                <textarea class="form-control rounded-0 @error('description') is-invalid @enderror" id="description" name="description"
                                     rows="3" required>{{ old('description') }}</textarea>
                                 @error('description')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -32,11 +35,11 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row text-white">
                         <div class="col-4">
                             <div class="form-group py-2">
-                                <label for="strength">Strength</label>
-                                <input type="number" max="20" class="form-control @error('strength') is-invalid @enderror" id="strength" name="strength"
+                                <label for="strength" class="fs-2 fw-semibold">Forza</label>
+                                <input type="number" min="0" max="20" class="form-control rounded-0 @error('strength') is-invalid @enderror" id="strength" name="strength"
                                     rows="3" required>{{ old('strength') }}
                                 @error('strength')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -45,8 +48,8 @@
                         </div>
                         <div class="col-4">
                             <div class="form-group py-2">
-                                <label for="defence">Defence</label>
-                                <input type="number" max="20" class="form-control @error('defence') is-invalid @enderror" id="defence" name="defence"
+                                <label for="defence" class="fs-2 fw-semibold">Difesa</label>
+                                <input type="number" min="0" max="20" class="form-control rounded-0 @error('defence') is-invalid @enderror" id="defence" name="defence"
                                     rows="3" required>{{ old('defence') }}
                                 @error('defence')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -55,8 +58,8 @@
                         </div>
                         <div class="col-4">
                             <div class="form-group py-2">
-                                <label for="speed">Speed</label>
-                                <input type="number" max="20" class="form-control @error('speed') is-invalid @enderror" id="speed" name="speed"
+                                <label for="speed" class="fs-2 fw-semibold">Velocita'</label>
+                                <input type="number" min="0" max="20" class="form-control rounded-0 @error('speed') is-invalid @enderror" id="speed" name="speed"
                                     rows="3" required>{{ old('speed') }}
                                 @error('speed')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -65,8 +68,8 @@
                         </div>
                         <div class="col-4">
                             <div class="form-group py-2">
-                                <label for="intelligence">Intelligence</label>
-                                <input type="number" max="20" class="form-control @error('intelligence') is-invalid @enderror" id="intelligence" name="intelligence"
+                                <label for="intelligence" class="fs-2 fw-semibold">Intelligenza</label>
+                                <input type="number" min="0" max="20" class="form-control rounded-0 @error('intelligence') is-invalid @enderror" id="intelligence" name="intelligence"
                                     rows="3" required>{{ old('intelligence') }}
                                 @error('intelligence')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -75,8 +78,8 @@
                         </div>
                         <div class="col-4">
                             <div class="form-group py-2">
-                                <label for="life">Life</label>
-                                <input type="number" max="20" class="form-control @error('life') is-invalid @enderror" id="life" name="life"
+                                <label for="life" class="fs-2 fw-semibold">Salute</label>
+                                <input type="number" min="0" max="20" class="form-control rounded-0 @error('life') is-invalid @enderror" id="life" name="life"
                                     rows="3" required>{{ old('life') }}
                                 @error('life')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -85,7 +88,7 @@
                         </div>
                     </div>
                     <div class="py-3">
-                        <button type="submit" class="btn btn-sm btn-md btn-primary fs-5 letter_spacing">Submit</button>
+                        <button type="submit" class="fs-3 p-0 px-2 rounded-0 letter_spacing">Genera</button>
                     </div>
                 </form>
             </div>
