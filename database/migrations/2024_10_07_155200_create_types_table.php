ee89124a33a2e74aafbb15b6dca12a765b5f0c65
Corfaya <lauracorrado0696@gmail.com>
2024-10-08 10:34:51 +0200

    [UPD] type migration

--- database/migrations/2024_10_07_155200_create_types_table.php
+++ database/migrations/2024_10_07_155200_create_types_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('image')->default('https://picsum.photos/id/1/200/300')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
