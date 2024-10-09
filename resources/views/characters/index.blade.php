a34567507abd0bedd688ae740650bee3998fbb5a
Corfaya <lauracorrado0696@gmail.com>
2024-10-09 21:34:35 +0200

    [UPD] index responsive rules added

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -3,10 +3,14 @@
 @section('content')
     <div class="bg-charachters">
         <div class="container">
-            <div class="row justify-content-center p-5">
-                <div class="col-12 col-md-12 col-lg-6 index-page bg-panel">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-8 index-page bg-panel my-5">
                     <div class="d-flex justify-content-center mb-3">
                         <h1>Lista dei personaggi</h1>
+                    </div>
+                    <div class="text-end mt-4 mb-2 ">
+                        <a href="{{ route('characters.create') }}" class="btn-personal btn-create"><i
+                                class="bi bi-plus-square-fill me-3"></i>Nuovo personaggio</a>
                     </div>
                     <!-- Lista dei personaggi -->
                     <div class="list-group">
@@ -15,16 +19,16 @@
                             <div
                                 class="list-group-item d-flex flex-column flex-md-row justify-content-between align-items-center">
                                 <!-- Nome del personaggio a sinistra -->
-                                <span class="fs-4">{{ $character->name }}</span>
+                                <span>{{ $character->name }}</span>
 
-                                <div class="mt-3 mt-md-0">
+                                <div class="mt-3 mt-md-0 d-none d-lg-block">
                                     <!-- Link per visualizzare i dettagli del personaggio -->
                                     <a href="{{ route('characters.show', ['character' => $character->id]) }}"
-                                        class="btn-personal btn-show me-2"><i class="bi bi-eye-fill"></i>Dettagli</a>
+                                        class="btn-personal btn-show"><i class="bi bi-eye-fill me-2"></i>Dettagli</a>
 
                                     <!-- Link per modificare il personaggio -->
                                     <a href="{{ route('characters.edit', ['character' => $character->id]) }}"
-                                        class="btn-personal btn-edit"><i class="bi bi-feather"></i>Riscrivi</a>
+                                        class="btn-personal btn-edit"><i class="bi bi-feather me-2"></i>Riscrivi</a>
 
                                     <!-- Form per eliminare il personaggio -->
                                     <form action="{{ route('characters.destroy', ['character' => $character->id]) }}"
@@ -35,12 +39,32 @@
                                             data-pg-name="{{ $character->name }}">Bandisci</button>
                                     </form>
                                 </div>
+                                {{-- qui i bottoni mobile --}}
+                                <div class="mt-3 mt-md-0 d-block d-lg-none">
+                                    <!-- Link per visualizzare i dettagli del personaggio -->
+                                    <a href="{{ route('characters.show', ['character' => $character->id]) }}"
+                                        class="btn-personal btn-show"><i class="bi bi-eye-fill"></i></a>
+
+                                    <!-- Link per modificare il personaggio -->
+                                    <a href="{{ route('characters.edit', ['character' => $character->id]) }}"
+                                        class="btn-personal btn-edit"><i class="bi bi-feather"></i></a>
+
+                                    <!-- Form per eliminare il personaggio -->
+                                    <form action="{{ route('characters.destroy', ['character' => $character->id]) }}"
+                                        method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn-personal del-btn"
+                                            data-pg-name="{{ $character->name }}"><i class="bi bi-trash2"></i></button>
+                                    </form>
+                                </div>
+                                
                             </div>
                         @endforeach
                     </div>
                     <div class="text-end mt-4 mb-2 ">
                         <a href="{{ route('characters.create') }}" class="btn-personal btn-create"><i
-                                class="bi bi-plus-square-fill"></i>Nuovo personaggio</a>
+                                class="bi bi-plus-square-fill me-3"></i>Nuovo personaggio</a>
                     </div>
                 </div>
             </div>
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -4,10 +4,14 @@
     <div class="bg-items">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="row justify-content-center p-5">
-                    <div class="col-12 col-md-12 col-lg-6 index-page bg-panel">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-8 index-page bg-panel my-5">
                         <div class="d-flex justify-content-center mb-3">
                             <h1>Lista oggetti</h1>
+                        </div>
+                        <div class="text-end mt-4 mb-2 ">
+                            <a href="{{ route('items.create') }}" class="btn-personal btn-create"><i
+                                    class="bi bi-plus-square-fill me-3"></i>Nuovo oggetto</a>
                         </div>
                         <!-- Lista degli oggetti -->
                         <div class="list-group">
@@ -16,16 +20,16 @@
                                 <div
                                     class="list-group-item d-flex flex-column flex-md-row justify-content-between align-items-center">
                                     <!-- Nome dell'oggetto a sinistra -->
-                                    <span class="fs-4">{{ $item->name }}</span>
+                                    <span">{{ $item->name }}</span>
 
-                                    <div class="mt-3 mt-md-0">
+                                    <div class="mt-3 mt-md-0 d-none d-lg-block">
                                         <!-- Link per visualizzare i dettagli dell'oggetto -->
                                         <a href="{{ route('items.show', ['item' => $item->id]) }}"
-                                            class="btn-personal btn-show"><i class="bi bi-eye-fill"></i>Dettagli</a>
+                                            class="btn-personal btn-show"><i class="bi bi-eye-fill me-2"></i>Dettagli</a>
 
                                         <!-- Link per modificare l'oggetto -->
                                         <a href="{{ route('items.edit', ['item' => $item->id]) }}"
-                                            class="btn-personal btn-edit"><i class="bi bi-feather"></i>Riscrivi</a>
+                                            class="btn-personal btn-edit"><i class="bi bi-feather me-2"></i>Riscrivi</a>
 
                                         <!-- Form per eliminare l'oggetto -->
                                         <form action="{{ route('items.destroy', ['item' => $item->id]) }}" method="POST"
@@ -33,7 +37,27 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn-personal del-btn"
-                                                data-pg-name="{{ $item->name }}">Bandisci</button>
+                                                data-pg-name="{{ $item->name }}"><i class="bi bi-trash2"></i>Bandisci</button>
+                                        </form>
+                                    </div>
+
+                                    {{-- qui i bottoni mobile --}}
+                                    <div class="mt-3 mt-md-0 d-block d-lg-none">
+                                        <!-- Link per visualizzare i dettagli dell'oggetto -->
+                                        <a href="{{ route('items.show', ['item' => $item->id]) }}"
+                                            class="btn-personal btn-show"><i class="bi bi-eye-fill"></i></a>
+
+                                        <!-- Link per modificare l'oggetto -->
+                                        <a href="{{ route('items.edit', ['item' => $item->id]) }}"
+                                            class="btn-personal btn-edit"><i class="bi bi-feather"></i></a>
+
+                                        <!-- Form per eliminare l'oggetto -->
+                                        <form action="{{ route('items.destroy', ['item' => $item->id]) }}" method="POST"
+                                            class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn-personal del-btn"
+                                                data-pg-name="{{ $item->name }}"><i class="bi bi-trash2"></i></button>
                                         </form>
                                     </div>
                                 </div>
@@ -41,7 +65,7 @@
                         </div>
                         <div class="text-end mt-4 mb-2 ">
                             <a href="{{ route('items.create') }}" class="btn-personal btn-create"><i
-                                    class="bi bi-plus-square-fill"></i>Nuovo oggetto</a>
+                                    class="bi bi-plus-square-fill me-3"></i>Nuovo oggetto</a>
                         </div>
                     </div>
                 </div>
