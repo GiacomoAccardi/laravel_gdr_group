5bbfbf95258bd98f8482aada40ee874555013b6e
pierobramanti <piero.bramanti94@gmail.com>
2024-10-10 16:55:38 +0200

    [UPD] created first layout footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,48 @@
+<footer class="bg-dark text-light py-2">
+    <div class="container">
+        <div class="row d-flex flex-nowrap">
+            <div class="col-4 border">
+                <a href="{{ route('about') }}" class="text-light text-decoration-none">
+                    <h5 class="fs-3">Su di noi</h5>
+                </a>
+                <p class="border">"Una compagnia di avventurieri audaci, legati dal destino, si unisce come cavalieri attorno alla Tavola Rotonda. Ogni membro porta con sé la propria storia e abilità, creando un'alleanza indissolubile."</p>
+            </div>
+            <div class="col-4 border">
+                <ul class="list-unstyled d-flex justify-content-between">
+                    <li><a href="{{ route('homepage') }}" class="text-light fs-4 text-decoration-none">Home</a></li>
+                    <li><a href="{{ route('contacts') }}" class="text-light fs-4 text-decoration-none">Contact</a></li>   
+                </ul>
+            </div>
+            <div class="col-4 border">
+                <h5 class="fs-2">Seguiteci</h5>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a href="#" class="text-light text-decoration-none fs-5">
+                            VoltoLibro <i class="bi bi-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-light text-decoration-none fs-5">
+                            Croce <i class="bi bi-twitter-x"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-light text-decoration-none fs-5">
+                            Iconografia <i class="bi bi-instagram"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item text-decoration-none  fs-5">
+                        <a href="#" class="text-light text-decoration-none">
+                            Il Vostro tubo <i class="bi bi-youtube"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center mt-3">
+                <p>&copy; Anno Domini 2024 - La compagnia del Procione - Tutti i diritti riservati.</p>
+            </div>
+        </div>
+    </div>
+</footer>
