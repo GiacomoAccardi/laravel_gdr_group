8d6f14451017cb568dc06027a5b1230582e7f585
Corfaya <lauracorrado0696@gmail.com>
2024-10-10 17:26:45 +0200

    [UPD] form styles changed

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -18,7 +18,7 @@
                                     placeholder="Your name">
                             </div>
                             <div class="col-12 col-md-6">
-                                <label class="form-label mt-3" for="mail">In quale modo ci è possibile contattarVi?</label>
+                                <label class="form-label mt-3" for="mail">In quale modo ci e' possibile contattarVi?</label>
                                 <input class="form-control" type="email" id="mail" name="mail"
                                     placeholder="example@web.com">
                             </div>
@@ -26,7 +26,7 @@
                         <div class="row gy-2">
                             <div class="col-12">
                                 <label class="form-label mt-3" for="comment">Un Vostro giudizio potrebbe meglio orientarci
-                                    nella ricerca del modo più consono per assisterVi</label>
+                                    nella ricerca del modo piu' consono per assisterVi</label>
                                 <textarea class="form-control" name="comment" id="comment" cols="30" rows="10"></textarea>
                             </div>
                         </div>
