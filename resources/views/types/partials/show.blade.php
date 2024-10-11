842fc6d3c9f8e40748c1937dd74fea986c36a5f1
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-10-11 16:05:17 +0200

    [UPD] Some adjustments to claas show view

--- resources/views/types/partials/show.blade.php
+++ resources/views/types/partials/show.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-12 bg-panel text-center p-5">
                     <h1 class="m-0">{{ $classinfoItem->name }}</h1>
-                    <img src="{{ $classinfoItem->image }}" alt="{{ $classinfoItem->name }}">
+                    <img src="{{ $classinfoItem->image }}" alt="{{ $classinfoItem->name }}" class="d250x mb-5">
                     <p class="fs-1 fw-bolder type-description mx-auto">{{ $classinfoItem->description }}</p>
                 </div>
             </div>
@@ -14,9 +14,9 @@
                 <div class="col-12">
                     <div class="d-flex justify-content-center">
                         <a href="{{ route('types.index') }}"
-                                    class="text-decoration-none fs-3 btn btn-sm rounded-0 back_button fw-semibold py-2">
-                                    <i class="bi bi-arrow-left"></i> Torno sui miei passi . .
-                                </a>
+                            class="text-decoration-none fs-3 btn btn-sm rounded-0 back_button fw-semibold py-2">
+                            <i class="bi bi-arrow-left"></i> Torno sui miei passi . .
+                        </a>
                     </div>
                 </div>
             </div>
