834947d05741bc7db46ca374bb11120a6e41f69c
Corfaya <lauracorrado0696@gmail.com>
2024-10-18 15:57:38 +0200

    [UPD] routes defined

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CharactersController;
+use App\Http\Controllers\Api\ItemsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/characters', [CharactersController::class, 'index'])->name('characters');
+Route::get('/items', [ItemsController::class, 'index'])->name('items');
